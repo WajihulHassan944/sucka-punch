ce0c9e18f65e6efd1747fd083b0f3204bf16c833
Usamali1 <usamaliaqat1264@gmail.com>
2025-05-02 06:39:20 +0500

    updated

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -33,7 +33,7 @@ export default function CheckoutPage() {
               <DeliveryMethod />
               <PaymentSection />
             </div>
-          </div>
+          </div> 
         </div>
       </div>
     </CheckoutProvider>
